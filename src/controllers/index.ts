6822510e0f457a14e18d32763e3b5c35cdddf267
Jonathan Medina <racieriver@gmail.com>
2021-11-02 13:43:39 -0600

    Se agrgan los elementos del MS de ventas

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,5 @@ export * from './factura-venta.controller';
 export * from './ping.controller';
 export * from './venta-factura.controller';
 
+export * from './venta.controller';
+export * from './factura.controller';
